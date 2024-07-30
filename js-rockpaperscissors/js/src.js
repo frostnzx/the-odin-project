39c5d377105506a52f5c3c4b3a9d962b05ab7462
Siwakorn <jeensiwakorn001@gmail.com>
2024-07-30 20:21:50 +0700

    new rps

--- js-rockpaperscissors/js/src.js
+++ js-rockpaperscissors/js/src.js
@@ -1,46 +1,78 @@
-let humanScore = 0 , computerScore = 0 ;
 
-function getComputerChoice() {
-    const choices = ["rock" , "paper" , "scissors"];
+const announce_label = document.querySelector("#announce-label");
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const btn_container = document.querySelector("#btn-container")
+
+const game_label = document.querySelector("#game-label");
+const player_label = document.querySelector("#player-label");
+const computer_label = document.querySelector("#computer-label");
+
+const choices = ["rock" , "paper" , "scissors"];
+let gameCount = 1 , playerScore = 0 , computerScore = 0 , pScore = 0 , cScore = 0 ;
+let playerChoice = "" ;
+
+
+btn_container.addEventListener("click" , (e) => {
+    playerChoice = e.target.id ;
+    game();
+});
+
+function game() {
+    gameCount++;
     let index = Math.floor(Math.random() * 3);
-    return choices[index];
+    let computerChoice = choices[index] ; 
+
+    if(playerChoice == computerChoice){
+        updateScore();
+        return;
+    }
+    switch(playerChoice){
+        case "rock" : 
+            computerChoice == "paper" ? computerScore++ : playerScore++;
+        break;
+        case "paper" : 
+            computerChoice == "scissors" ? computerScore++ : playerScore++;
+        break;
+        case "scissors" : 
+            computerChoice == "rock" ? computerScore++ : playerScore++;
+        break;
+    }
+    updateScore();
+    if(playerScore == 5){
+        confirm("Player won the match!");
+        reset();
+    }
+    else if(computerScore == 5){
+        confirm("Computer won the match!");
+        reset();
+    }
+    updateScore();
 }
-function playRound(humanChoice , computerChoice) {
-    let winner = "tied";
-    if(humanChoice == "rock") {
-        if(computerChoice == "scissors") {
-            winner = "human"
-        }
-        else if(computerChoice == "paper") {
-            winner = "computer"
-        }
+
+function updateScore() {
+    game_label.innerHTML = "Game : " + gameCount.toString();
+    player_label.innerHTML = "Player : " + playerScore.toString();
+    computer_label.innerHTML = "Computer : " + computerScore.toString();
+
+    if(playerScore > pScore) {
+        announce_label.innerHTML = "Player is winning!" ;
     }
-    else if(humanChoice == "paper") {
-        if(computerChoice == "rock") {
-            winner = "human"
-        }
-        else if(computerChoice == "scissors") {
-            winner = "computer"
-        }
+    else if(computerScore > cScore) {
+        announce_label.innerHTML = "Computer is winning!" ;
     }
-    else if(humanChoice == "scissors") {
-        if(computerChoice == "paper") {
-            winner = "human"
-        }
-        else if(computerChoice == "rock") {
-            winner = "computer"
-        }
-    }
-    if(winner == "human") {
-        humanScore++ ;
-    }
-    else if(winner == "computer"){
-        computerScore++;
-    }
-    console.log("The winner is " , winner);
-    console.log("human score : " , humanScore , "  ,  computer score : " , computerScore);
+
+    pScore = playerScore;
+    cScore = computerScore;
 }
-for(let i=1;i<=5;i++){
-    let input = prompt("What is your choice?");
-    playRound(input , getComputerChoice());
+
+function reset() {
+    gameCount = 0 , playerScore = 0 , computerScore = 0 , pScore = 0 , cScore = 0 ;
+    playerChoice = "" ;
+    announce_label.innerHTML = "Who is winning" ;
+    game_label = "Game : 0";
+    player_label = "Player : 0";
+    computer_label = "Computer : 0";
 }
